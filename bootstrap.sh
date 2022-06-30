188a653bd35332dfb5a6eb13f28d6e56714c7ef5
Vladimir Katardjiev <github@onetime.d2dx.com>
2022-06-30 10:49:11 -0700

    [NONE] Squash

---
+++ bootstrap.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+mkdir -p ~/.m2/repository/org/jacoco/org.jacoco.agent/0.8.8
+cd ~/.m2/repository/org/jacoco/org.jacoco.agent/0.8.8
+wget https://repo1.maven.org/maven2/org/jacoco/org.jacoco.agent/0.8.8/org.jacoco.agent-0.8.8.jar
